8d5171f67999fb8c7a2b36da2f897dc8dd05588e
BelzDaniel <BelzDaniel6@gmail.com>
2024-08-09 11:33:03 +0300

    partial : starting react query

---
+++ src/api/tasks.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { Task } from "../types/item";
+
+export const getAllTasks = async (param: boolean) => {
+  const response = await axios.get(import.meta.env.VITE_API_URL, {
+    params: param ? { param } : {},
+  });
+  return response.data as Task[];
+};
+
+export const getTask = async (id: number) => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/tasks/${id}`
+  );
+  return response.data as Task;
+};
+
+export const createTask = async (task: Task) => {
+  const response = await axios.post(import.meta.env.VITE_API_URL, task);
+  return response.data as Task;
+};
+
+export const updateTask = async (id: number, task: Task) => {
+  const response = await axios.patch(
+    `${import.meta.env.VITE_API_URL}/${id}`,
+    task
+  );
+
+  return response.data as Task;
+};
+
+export const deleteTask = async (id: number) => {
+  const response = await axios.delete(`${import.meta.env.VITE_API_URL}/${id}`);
+
+  return response.data as Task;
+};
--- src/types/item.ts
+++ src/types/item.ts
@@ -1,4 +1,4 @@
-export type Item = {
+export type Task = {
   id: number;
   item: string;
   checked: boolean;
